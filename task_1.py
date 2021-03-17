10cb72fafe1a20fdb8b443063ecad5fdc1a134c1
antonshe84 <antonshe84@gmail.com>
2021-03-17 23:37:54 +0300

    edit

--- task_1.py
+++ task_1.py
@@ -11,3 +11,49 @@
 завершать скрипт.
 """
 
+from time import sleep
+
+
+class TrafficLight:
+    def __init__(self, auto=True, color="red", time_d=10):
+        if auto:
+            self.svet_param = [(7, 1, 0, 0),
+                               (2, 1, 3, 0),
+                               (10, 0, 0, 2),
+                               (2, 0, 3, 0)
+                               ]
+        else:
+            if color == "red":
+                self.svet_param = [(time_d, 1, 0, 0)]
+            elif color == "yellow":
+                self.svet_param = [(time_d, 0, 3, 0)]
+            elif color == "green":
+                self.svet_param = [(time_d, 0, 0, 2)]
+            else:
+                self.svet_param = [(time_d, 1, 3, 2)]
+
+        __color = (0, 0, 0)
+        __time = time_d
+
+    def running(self):
+        for i in self.svet_param:
+            __time = i[0]
+            __color = (i[1], i[2], i[3])
+            print(f"\b" * 10, end="")
+            print(f"\033[4{__color[0]}m   ", end="")
+            print(f"\033[4{__color[1]}m   ", end="")
+            print(f"\033[4{__color[2]}m   ", end="")
+            sleep(__time)
+
+
+if __name__ == '__main__':
+    svetofor = TrafficLight()
+    for _ in range(2):
+        svetofor.running()
+
+    svetofor = TrafficLight(False, "red", 10)
+    svetofor.running()
+
+    svetofor = TrafficLight(False, "green", 10)
+    svetofor.running()
+
